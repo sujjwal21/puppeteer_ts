811c96c304412567e11971689e51bb8149f242bf
sujjwal21 <sujjwal85@gmail.com>
2023-04-30 21:52:04 +0530

    Puppeteer

---
+++ index.ts
@@ -0,0 +1,56 @@
+const puppeteer = require("puppeteer")
+const fs = require("fs")
+import { parse } from "path"
+import { Browser } from "puppeteer"
+
+const url = "https://books.toscrape.com"
+
+const main = async () => {
+    const browser: Browser = await puppeteer.launch({ headless: true })
+    // console.log(123)
+    const page = await browser.newPage()
+    await page.goto(url)
+    await page.screenshot({path:"btoa.jpg"})
+
+    const bookData = await page.evaluate((url) => {
+
+        const convertPrice = (price: String) => {
+            return parseFloat(price.replace("£", ""))
+        }
+        const convertRating = (rating: String) => {
+            switch (rating) {
+                case "One":
+                    return 1
+                case "Two":
+                    return 2
+                case "Three":
+                    return 3
+                case "Four":
+                    return 4
+                case "Five":
+                    return 5
+                default:
+                    return 0
+            }
+        }
+
+        const bookPods = Array.from(document.querySelectorAll(".product_pod"))
+        const data = bookPods.map((book: any) => ({
+            title: book.querySelector("h3 a").getAttribute("title"),
+            price: convertPrice(book.querySelector(".price_color").innerText),
+            imgSrc: url + book.querySelector("img").getAttribute("src"),
+            rating: convertRating(book.querySelector(".star-rating").classList[1])
+        }))
+        return data
+    }, url)
+
+    // console.log(bookData)
+
+    await browser.close()
+    fs.writeFile("data.json", JSON.stringify(bookData), (err: any) => {
+        if (err) throw err
+        console.log("Successfully Saved Json Data")
+    })
+}
+
+main()
